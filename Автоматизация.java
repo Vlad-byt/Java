30205da2f395e0d78ef35af5b99a77d18f91fa4f
Vlad-byt <80098400+Vlad-byt@users.noreply.github.com>
2021-12-09 03:57:29 +0300

    Update Автоматизация.java

--- Автоматизация.java
+++ Автоматизация.java
@@ -4,103 +4,195 @@ import java.util.*;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Arrays;
 
 public class Main {
-    
-public static void main(String[] args){
-        HashMap<String, String> passportsAndNames = new HashMap<>();
-        Auth auth = new Server(passportsAndNames);
+    public static void main(String[] args) {
+        HashMap<String, byte []> reg = new HashMap<>();
+        Server server = new ServerImpl(reg);
+        Auth auth = new AuthImpl(reg);
         auth.Registrate();
-        auth.Sign_In();
-    }
+        auth.SignIn(server.TakeAccount());
+        auth.ChangePassword(server.TakeAccount());
+        auth.DeleteAccount(server.TakeAccount());
+        server.Display();
+}
 }
 
 class Auth {
-    HashMap<String, String> passportsAndNames;
+    HashMap<String, byte []> reg;
     
-    public Auth(HashMap<String, String> passportsAndNames){
-        this.passportsAndNames = passportsAndNames;
+    public Auth(HashMap<String, byte []> reg){
+        this.reg = reg;
     }
     
-    public void Registrate(){
-        Pattern pattern1 = Pattern.compile("\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*\\.\\w{2,4}");
-        Pattern pattern2 = Pattern.compile("^\\+?[0-9\\-\\s]*$");
-        if (passportsAndNames.isEmpty()) {
-            System.out.print("Вы ещё не зарегестрированы!\n");
-            Scanner sc = new Scanner(System.in);
-            String login, password;
-            
-            System.out.print("Введите почту или телефон: ");
-            login = sc.nextLine();
-            Matcher matcher1 = pattern1.matcher(login);
-            Matcher matcher2 = pattern2.matcher(login);
-            
-            boolean matches1 = matcher1.matches();
-            boolean matches2 = matcher2.matches();
-            if (matches1 == true) {
-                System.out.print("Введите пароль: ");
-                password = sc.nextLine();
-            
-                this.passportsAndNames.put(login, password);
-                System.out.print(this.passportsAndNames);
+     public void Registrate(){
+         Pattern pattern1 = Pattern.compile("\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*\\.\\w{2,4}");
+         Server server = new ServerImpl(reg);
+         
+             System.out.println("Хотите зарегестрироваться? Если да напишите Y");
+             Scanner sc = new Scanner(System.in);
+             String login, password, answer;
+             answer = sc.nextLine();
+             
+             if (answer.toUpperCase().equals("Y")) {
+                  System.out.println("Введите свой email: ");
+                  login = sc.nextLine();
+                  Matcher matcher1 = pattern1.matcher(login);
+                  boolean matches1 = matcher1.matches();
+             
+                   if (matches1 == true) {
+                   System.out.println("Введите пароль: ");
+                   password = sc.nextLine();
+                   byte[] byt = password.getBytes();
+                   this.reg.put(login, byt);
+                   server = new ServerImpl(reg);
+                   server.Display();
+                } else {System.out.println("Почта не найдена!");}
+         
+         }
+             else {
+                  System.out.println("Имеете учётную запись? Пройдите регистрацию: ");
+                  Auth auth = new AuthImpl(reg);
+                  auth.SignIn(server.TakeAccount());
+             }
+     }
+     
+     public void SignIn(HashMap<String, byte []> log){
+         Scanner sc = new Scanner(System.in);
+         String login, password;
+         System.out.println("Введите логин: ");
+         login = sc.nextLine();
+         System.out.println("Введите пароль: ");
+         password = sc.nextLine();
+         try {
+              byte [] key = reg.get(login);
+              String string = new String(key);
+           if (string.equals(password)) {
+               System.out.println("Добро пожаловать!");
+             } else {System.out.print("Неверный логин или пароль!");}
+             
+         }
+         catch (Exception exception) {
+             System.out.println("Неверный логин или пароль!");
+         }
+        
+    }
+     
+     public void ChangePassword(HashMap<String, byte []> dict){
+         Scanner sc = new Scanner(System.in);
+         String password;
+         System.out.println("Введите старый пароль: ");
+         password = sc.nextLine();
+         
+         Set<String> keys = dict.keySet();
+         ArrayList<byte []> values = new ArrayList<>(dict.values());
+         
+         Iterator<String> it = keys.iterator();
+         Iterator<byte []> it2 = values.iterator();
+         
+         while(it.hasNext()){
+           String key = it.next();
+           
+           while(it2.hasNext()){
+           byte[] value = it2.next();
+           String string = new String(value);
+           
+           if (string.equals(password)){
+               System.out.println("Введите новый парль: ");
+               String newpassword = sc.nextLine();
+               byte[] byt = newpassword.getBytes();
+               dict.put(key, byt);
+               Server server = new ServerImpl(dict);
+               System.out.println("Пароль успешно изменён!");
+               break;
+           }
+           else {System.out.println("Неверный старый пароль!"); break;}
+             
             }
-            else if (matches2 == true){
-                System.out.print("Введите пароль: ");
-                password = sc.nextLine();
-            
-                this.passportsAndNames.put(login, password);
-                System.out.print(this.passportsAndNames);
-            } 
-            else { System.out.println("Ошибка"); }
+         }
+     }
+     
+     public void DeleteAccount(HashMap<String, byte []> dict){
+         Scanner sc = new Scanner(System.in);
+         Server server = new ServerImpl(dict);
+         String login;
+         System.out.println("Введите логин: ");
+         login = sc.nextLine();
+         try {
+             Set<String> keys = dict.keySet();
+             Iterator<String> it = keys.iterator();
+             while(it.hasNext()){
+                 String key = it.next();
+                 if (key.equals(login)) {
+                      server.Remove(key);
+                      System.out.println("Запись успешно удалена!");
+                 }
+         }
+         }
+         catch (Exception exception) {
+             System.out.println("Неверный логин!");
+         }
+    }
+}
+
+class AuthImpl extends Auth {
+    
+    public AuthImpl(HashMap<String, byte []> reg) {
+              super(reg);
+          }
+}
+
+
+class Server {
+    HashMap<String, byte []> reg;
+    
+    public Server(HashMap<String, byte []> reg){
+        this.reg = reg;
+    }
+    
+    public boolean IsNull(){
+        if (this.reg.isEmpty()){
+            return false;
+        } else { return true;}
+    }
+    
+    public HashMap<String, byte []> TakeAccount(){
+        return this.reg;
+    }
+    
+     public void Display(){
+         if (this.reg.isEmpty()){ 
+             System.out.println("Создайте аккаунт!");
+         }
+    else {
+        Set<String> keys = this.reg.keySet();
+        Iterator<String> it = keys.iterator();
+        while(it.hasNext()){
+           String key = it.next();
+           System.out.println("Логин: " + key + " Пароль: *****");
         }
-        else {System.out.println("Вы уже зарегестрированы");}
     }
         
-        public void Sign_In(){
-            Pattern pattern1 = Pattern.compile("\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*\\.\\w{2,4}");
-            Pattern pattern2 = Pattern.compile("^\\+?[0-9\\-\\s]*$");
-            Scanner sc = new Scanner(System.in);
-            String login, password;
-            
-            System.out.print("Введите почту или телефон: ");
-            login = sc.nextLine();
-            Matcher matcher1 = pattern1.matcher(login);
-            Matcher matcher2 = pattern2.matcher(login);
-            
-            boolean matches1 = matcher1.matches();
-            boolean matches2 = matcher2.matches();
-            if (matches1 == true) {
-                System.out.print("Введите пароль: ");
-                password = sc.nextLine();
-                for (String keys : this.passportsAndNames.keySet()) {
-                    String key = keys;
-                }
-                for (String values : this.passportsAndNames.values()) {
-                    String value = values;
-                }
-                if (login == key && password == value){
-                    System.out.print("Вход успешно выполнен!");
-                } else {System.out.print("Ошибка входа");}
-            }
-            else if (matches2 == true){
-                System.out.print("Введите пароль: ");
-                password = sc.nextLine();
-                for (String keys : this.passportsAndNames.keySet()) {
-                    String key = keys;
-                }
-                for (String values : this.passportsAndNames.values()) {
-                    String value = values;
-                }
-                if (login == key && password == value){
-                    System.out.print("Вход успешно выполнен!");
-                } else {System.out.print("Ошибка входа");}
-            } 
-            else { System.out.println("Ошибка"); }
-        }
+}
+
+    public void Remove(String name){
+        this.reg.remove(name);
     }
-    
-   class Server extends Auth{
-          public Server(HashMap<String, String> passportsAndNames) {
-              super(passportsAndNames);
+}
+
+class ServerImpl extends Server {
+   /*
+    private static Server _instance = null;
+
+    public static Server getInstance() {
+        if (_instance == null)
+            _instance = new ServerImpl();
+        return _instance;
+    }*/
+    public ServerImpl(HashMap<String, byte []> reg) {
+              super(reg);
           }
-    } 
+}
